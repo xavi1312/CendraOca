66863ca083a5452f01ee17d54226cc6a07ffaa3b
Xavi <xavierisachalemany@gmail.com>
2018-11-26 23:50:35 +0100

    millores

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 class jugador{
     constructor(actual){
         this.celdaActual = actual; 
-        this.penalizado = false;
+        this.penalizado = 0;
     }
     tirarDado(){
         var random = Math.round(Math.random() * (6 - 1)) + 1 ;
@@ -22,7 +22,7 @@ class jugador{
         return this.penalizado;
     }
     setPenalizado(penalizado){
-        this.penalizado = penalizado;
+        this.penalizado += penalizado;
     }
 }
 var jugadorUno = new jugador(1);
@@ -39,54 +39,52 @@ function nuevoTurno(){
     tirar(turno, ficha);
 }
 function tirar(jugador, ficha){
-    const ultimaCalda = 63;
     var numRan = jugador.tirarDado();
     $("#dado").text(numRan);
-
-    mover(ficha, jugador.getceldaActual(), numRan)
+    
+    mover(ficha, jugador,numRan);
+    celdaEspecial(ficha,jugador);
     
 }
-function mover(ficha,inicio, fin){
-    for(let i=inicio; i<=fin; i++){
-        $(ficha).appendTo($('.tablero__celda--'+i));
+function mover(ficha,jugador, fin){
+    const ultimaCelda = 63;
+    var numero;
+    if(jugador.getceldaActual()+fin > ultimaCelda){
+        fin = (jugador.getceldaActual()+fin) - ultimaCelda;
     }
-}
-function casellaEspecial(ficha,celda, objeto){
-    turnoJugadorUno = false;
-    if(
-        celda == 5 ||  
-        celda == 14 ||
-        celda == 23 ||
-        celda == 32 ||
-        celda == 41 ||
-        celda == 50
-        ){
-            objeto.setCeldaActual(celda+4);
-            mover(ficha, celda+4);
-            turnoJugadorUno = true;
-        }
-    else if(
-        celda == 9  ||
-        celda == 18 ||
-        celda == 27 ||
-        celda == 36 ||
-        celda == 45 ||
-        celda == 54
+    for(let i=1; i<=fin;
+        i++
     ){
-        objeto.setCeldaActual(celda+5);
-        mover(ficha, celda+5);
-        turnoJugadorUno = true;
+        numero = + (Number.parseInt(jugador.getceldaActual())+i);
+        
+        $(ficha).appendTo($('.tablero__celda--'+numero));
     }
-    else if(
-        celda == 6 ||
-        celda == 12
-        ){
-            objeto.setCeldaActual(19);
-            mover(ficha, 19);
-        }
-    else if(celda == 52){
+    console.log(numero);
+    jugador.setCeldaActual(numero)
+}
+function celdaEspecial(ficha,jugador){
+    var celda = jugador.getceldaActual();
+    switch(celda){
+        case 5 : mover(ficha, jugador,4);  break;
+        case 14: mover(ficha, jugador,4);  break;
+        case 23: mover(ficha, jugador,4);  break;
+        case 32: mover(ficha, jugador,4);  break;
+        case 41: mover(ficha, jugador,4);  break;
+        case 50: mover(ficha, jugador,4);  break;
 
-    }else if(celda == 58){
-        mover(ficha, 1)
+        case 9 : mover(ficha, jugador,5);  break;
+        case 18: mover(ficha, jugador,5);  break;
+        case 27: mover(ficha, jugador,5);  break;
+        case 36: mover(ficha, jugador,5);  break;
+        case 45: mover(ficha, jugador,5);  break;
+        case 54: mover(ficha, jugador,5);  break;
+
+        case 6 : mover(ficha, jugador,13); break;
+
+        case 12: mover(ficha, jugador,7);  break;
+
+        case 52: ; break;
+
+        case 58: ; break;
     }
 }

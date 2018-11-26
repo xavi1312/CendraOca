f82b11296c9f469773c125489ae406777c9b342e
Xavi <xavierisachalemany@gmail.com>
2018-11-26 07:54:22 +0100

    js

--- js/index.js
+++ js/index.js
@@ -26,6 +26,7 @@ function tirarUno(){
     dadoUno.setCeldaActual(dadoUno.getceldaActual()+num);
     let classe = '.tablero__celda--'+dadoUno.getceldaActual();
     $('#ficha1').appendTo($(classe))
+    casellaEspecial(dadoUno.getceldaActual());
 }
 function tirarDos(){
     var num = dadoDos.tirarDado();
@@ -33,4 +34,38 @@ function tirarDos(){
     dadoDos.setCeldaActual(dadoDos.getceldaActual()+num);
     let classe = '.tablero__celda--'+dadoDos.getceldaActual();
     $('#ficha2').appendTo($(classe))
+}
+
+function casellaEspecial(celda){
+    if(
+        celda == 5 ||
+        celda == 9 ||
+        celda == 14 ||
+        celda == 18 ||
+        celda == 23 ||
+        celda == 27 ||
+        celda == 32 ||
+        celda == 36 ||
+        celda == 41 ||
+        celda == 44 ||
+        celda == 45 ||
+        celda == 54 ||
+        celda == 50 ||
+        celda == 59
+        ){
+            alert("Has caigut a la oca, TORNA A TIRAR!!")
+        }
+    else if(
+        celda == 6 ||
+        celda == 42
+        ){
+            
+        }
+    else if(celda == 19){
+
+    }else if(celda == 52){
+
+    }else if(celda == 58){
+        
+    }
 }
